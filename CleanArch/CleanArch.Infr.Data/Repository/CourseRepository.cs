f18009cbfe41ed5bc141cdbccec14712c02504e6
Mrn-ilia <Mrn-ilia@outlook.com>
2020-12-15 10:07:47 +0330

    Commit Implementation of Service and Repository GetCourses Methods

---
+++ CleanArch/CleanArch.Infr.Data/Repository/CourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infr.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Infr.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UninersityDBContext _ctx;
+        public CourseRepository(UninersityDBContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return _ctx.Courses;
+        }
+    }
+}
